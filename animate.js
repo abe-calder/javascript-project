29fbfa48c3f4f309aa59c9713ddd6939228294e0
Abe Calder <abecalder1@gmail.com>
2025-10-19 21:37:12 +1300

    remove comments

--- animate.js
+++ animate.js
@@ -4,7 +4,7 @@ const [ $logo ] = utils.$('.logo.js');
 const [ $button ] = utils.$('button');
 let rotations = 0;
 
-// Created a bounce animation loop
+
 animate('.logo.js', {
   scale: [
     { to: 1.25, ease: 'inOut(3)', duration: 200 },
@@ -14,13 +14,12 @@ animate('.logo.js', {
   loopDelay: 250,
 });
 
-// Make the logo draggable around its center
 createDraggable('.logo.js', {
   container: [0, 0, 0, 0],
   releaseEase: createSpring({ stiffness: 200 })
 });
 
-// Animate logo rotation on click
+
 const rotateLogo = () => {
   rotations++;
   $button.innerText = `rotations: ${rotations}`;
